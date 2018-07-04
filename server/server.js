0a2f9516ee837c531af798ca0d615771cc942c7b
Ramon Miklus <r.miklus@sportradar.com>
2018-07-04 20:15:35 +0200

    Connect and disconnect event handlers for Socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
+const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
+var io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// Web Socket
+io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
 
